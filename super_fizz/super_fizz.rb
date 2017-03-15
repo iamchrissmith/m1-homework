9e45887730696c5339ab1458fa60df9d1044c70a
Chris Smith <42@chrisryansmith.com>
2017-03-15 15:44:53 -0600

    redo using % 7 instead of separate methods

--- super_fizz/super_fizz.rb
+++ super_fizz/super_fizz.rb
@@ -3,23 +3,11 @@ require 'pry'
 # Iterate through the numbers 0 through 1000
 
 class Fizz
-  def divisible_3(number)
-    true if number % 3 == 0
-  end
-
-  def divisible_5(number)
-    true if number % 5 == 0
-  end
-
-  def divisible_7(number)
-    true if number % 7 == 0
-  end
-
   def evaluate(number)
     result = ""
-    result += "Super" if divisible_7(number)
-    result += "Fizz" if divisible_3(number)
-    result += "Buzz" if divisible_5(number)
+    result += "Super" if number % 7 == 0
+    result += "Fizz" if number % 3 == 0
+    result += "Buzz" if number % 5 == 0
     result = number if result == ''
     result
   end
